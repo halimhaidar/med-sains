3fcbb608c173b8c7cbc54597f2183e51b3bb82b1
Revirdi <ardwiyanrevirdi99@gmail.com>
2024-07-31 00:56:00 +0700

    adjust: login-register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -44,16 +44,6 @@
                                 placeholder="xxxxxxx" required>
                         </div>
                         <div>
-                            <label for="role"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
-                            <select id="role" name="role"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                <option selected disabled>Choose Role</option>
-                                <option value="superadmin">superadmin</option>
-                                <option value="admin">admin</option>
-                            </select>
-                        </div>
-                        <div>
                             <label for="password"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <div class="relative">
