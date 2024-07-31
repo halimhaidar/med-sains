b17b683d7b2e891604a14517b46bc0c05e5e0d6b
Revirdi <ardwiyanrevirdi99@gmail.com>
2024-08-01 00:26:56 +0700

    adjust: edit sales PIC

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -97,6 +97,18 @@
                     </option>
                 </select>
             </div>
+            <div class="col-span-2 ">
+                <label for="pic" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sales PIC</label>
+                <select id="pic" name="pic"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                    <option selected disabled>Select Sales PIC</option>
+                    @foreach ($users as $user)
+                        <option value="{{ $user->fullname }}" {{ $company->pic == $user->fullname ? 'selected' : '' }}>
+                            {{ $user->fullname }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
             <div class="col-span-2">
                 <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
                 <textarea id="address" name="address" rows="4"
